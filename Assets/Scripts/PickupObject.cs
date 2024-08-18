8b13b26c1dbf85f16b4dc2d643f30bf6594b745c
jheess <jheess@students.uni-mainz.de>
2024-08-18 13:26:51 +0200

    Add pickup point relative to camera (does not work as intended)

--- Assets/Scripts/PickupObject.cs
+++ Assets/Scripts/PickupObject.cs
@@ -6,22 +6,23 @@ public class PickupObject : MonoBehaviour
 {
     // Start is called before the first frame update
     public Rigidbody rigidBody;
-
     void Start()
     {
         gameObject.layer = LayerMask.NameToLayer("PickupObject");
     }
 
-    public void Pickup(Transform player)
+    public void Pickup(Transform player, Transform target)
     {
         // attach Object to player camera
         transform.SetParent(player, true);
         rigidBody.constraints = RigidbodyConstraints.FreezePosition;
+        transform.position = target.position;
         rigidBody.useGravity = false;
     }
 
     public void Drop()
     {
+        Debug.Log("Drop");
         // detatch Object form camera
         transform.SetParent(null);
         rigidBody.constraints = RigidbodyConstraints.None;
