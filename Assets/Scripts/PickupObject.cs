a8c11702e1ed324c030935c402b1afa9d9cbbb94
jheess <jheess@students.uni-mainz.de>
2024-08-19 11:39:33 +0200

    Make Objects liftable through portal

--- Assets/Scripts/PickupObject.cs
+++ Assets/Scripts/PickupObject.cs
@@ -30,7 +30,6 @@ public class PickupObject : MonoBehaviour
         transform.SetParent(player, true);
         parentTransform = player;
         targetTransform = target;
-        transform.position = target.position;
         rigidBody.useGravity = false;
         rigidBody.drag = 10;
     }
