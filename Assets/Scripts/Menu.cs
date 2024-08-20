8a3c0bfaf5317ce101ddb8f01c016d8641991b52
Sebastian Hemberger <shemberg@students.uni-mainz-de>
2024-08-20 10:59:47 +0200

    add new assets

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public void Play()
+    {
+        Debug.Log("Play");
+    }
+
+    public void Options()
+    {
+        
+    }
+
+    public void Credits()
+    {
+        
+    }
+
+    public void Exit()
+    {
+        
+    }
+}
