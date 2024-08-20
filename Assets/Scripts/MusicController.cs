dfd5405f88a33457c7f665d9827e979f39801955
efraimdahl <efraimdahl@uchicago.edu>
2024-08-20 10:16:23 +0200

    adding jumping and landing sounds, fixing existing sounds

--- Assets/Scripts/MusicController.cs
+++ Assets/Scripts/MusicController.cs
@@ -9,56 +9,79 @@ public class MusicController : MonoBehaviour
     public string gunInstance = "event:/PortalGun";
     public string musicInstance = "event:/Music";
     public string stepInstance = "event:/Footsteps";
+    public string jumpInstance = "event:/Jump";
+    public string landInstance = "event:/Land";
     FMOD.Studio.EventInstance stepEvent;
     FMOD.Studio.EventInstance gunEvent;
     FMOD.Studio.EventInstance musicEvent;
+    FMOD.Studio.EventInstance landEvent;
+    FMOD.Studio.EventInstance jumpEvent;
     public bool playingFootsteps = false;
     private float footstepDistanceTime = 0.5f;
 
     public float randomActionTime = 20f;
+    private float size = 1f;
     // Start is called before the first frame update
     void Start()
     {
         FMODUnity.RuntimeManager.CoreSystem.mixerSuspend();
         FMODUnity.RuntimeManager.CoreSystem.mixerResume();
-        gunEvent = FMODUnity.RuntimeManager.CreateInstance(gunInstance);
-        gunEvent.setParameterByName("Size", 1);
+        
         musicEvent = FMODUnity.RuntimeManager.CreateInstance(musicInstance);
         musicEvent.setParameterByName("Action", 0);
         musicEvent.start();
+        
         StartCoroutine(musicSetter());
-        stepEvent = FMODUnity.RuntimeManager.CreateInstance(stepInstance);
         StartCoroutine(footsteps());
+        
+        
     }
     public void triggerGunAudio()
     {
+        gunEvent = FMODUnity.RuntimeManager.CreateInstance(gunInstance);
+        gunEvent.setParameterByName("Size", size);
         gunEvent.start();
         gunEvent.release();
     }
 
     public void triggerFootstep()
     {
+        stepEvent = FMODUnity.RuntimeManager.CreateInstance(stepInstance);
         stepEvent.start();
         stepEvent.release();
     }
+
+    public void triggerLanding()
+    {
+        landEvent = FMODUnity.RuntimeManager.CreateInstance(landInstance);
+        landEvent.start();
+        landEvent.release();
+
+    }
+
+
+    public void triggerJump()
+    {
+        jumpEvent = FMODUnity.RuntimeManager.CreateInstance(jumpInstance);
+        gunEvent.setParameterByName("Size", size);
+        jumpEvent.start();
+        jumpEvent.release();
+    }
     public void setSize(float progress)
     {
-        gunEvent.setParameterByName("Size", progress);
+        size = progress;
+        
     }
     IEnumerator footsteps()
     {
         while (true)
         {
+            //Debug.Log("playing Footsteps: "+ playingFootsteps);
             if (playingFootsteps)
             {
                 triggerFootstep();
-                yield return new WaitForSeconds(footstepDistanceTime);
-
             }
-            else
-            {
-                yield return new WaitForSeconds(footstepDistanceTime);
-            }
+            yield return new WaitForSeconds(footstepDistanceTime);
         }
     }
 
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -175,8 +175,10 @@ public class PlayerController : PortalableObject
         {
             jumpForces = Vector3.up * jumpForce;
             animation.Jump();
+            music.triggerJump();
         }
         rb.AddForce(jumpForces,ForceMode.VelocityChange);
+        
     }
 
     void Look()
@@ -206,7 +208,6 @@ public class PlayerController : PortalableObject
         Vector3.ClampMagnitude(velocityChange, maxForce);
         rb.AddForce(velocityChange, ForceMode.VelocityChange);
         //Check if movement is horizontal:
-        Debug.Log(move);
         if(Mathf.Abs(move.x)> threshold || Mathf.Abs(move.y) > threshold)
         {
             music.playingFootsteps=true;
@@ -323,8 +324,11 @@ public class PlayerController : PortalableObject
         if (state && !grounded)
         {
             animation.Land();
+            music.triggerLanding();
+            music.triggerFootstep();
         }
         grounded = state;
+
     }
     
     public override void Warp()
